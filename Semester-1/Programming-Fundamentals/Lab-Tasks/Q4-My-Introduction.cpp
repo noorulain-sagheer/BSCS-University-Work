d39c6db2fb2051ac9d8e8c9609584256db3b0059
noorulain-sagheer <noorulainsagheer1234@gmail.com>
2026-02-10 14:32:03 +0500

    Create Q4-My-Introduction.cpp

---
+++ Semester-1/Programming-Fundamentals/Lab-Tasks/Q4-My-Introduction.cpp
@@ -0,0 +1,31 @@
+//Your need to print the formatted output like shown below: 
+ 
+//*** My Introduction ***  
+//*Name: Your Name *  
+//*DOB: Your Birthdate *  
+//*City: Your city * *Prog: Your Prog *  
+//* 
+ 
+//The End 
+ 
+//*
+
+//Code:
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "*** My Introduction ***" << endl;
+    cout << "*Name: Noor-Ul-Ain Sagheer *" << endl;
+    cout << "*DOB: 17/01/2003 *" << endl;
+    cout << "*City: Islamabad * *Prog: BSCS *" << endl;
+    cout << "*" << endl;
+    cout << endl;
+    cout << "The End" << endl;
+    cout << "*" << endl;
+
+    return 0;
+}
