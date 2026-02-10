028b7e0285135c925e04e580b0c962528bfcab16
noorulain-sagheer <noorulainsagheer1234@gmail.com>
2026-02-10 14:20:27 +0500

    Update Q1-Area-of-Circle.cpp

--- Semester-1/Programming-Fundamentals/Lab-Tasks/Q3-Area-of-Circle.cpp
+++ Semester-1/Programming-Fundamentals/Lab-Tasks/Q3-Area-of-Circle.cpp
@@ -1,1 +1,18 @@
 
+//Q1. Write a program that takes radius of circle from user and calculate the area of circle using the following formula. Area = πr2.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    float radius, area;
+
+    cout << "Enter radius of circle: ";
+    cin >> radius;
+
+    area = 3.1416 * radius * radius;
+
+    cout << "Area of circle is: " << area << endl;
+
+    return 0;
+}
