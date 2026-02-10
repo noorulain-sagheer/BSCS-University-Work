babd9aa8032caa4a7a72ee7d18fb8e7d8aef3265
noorulain-sagheer <noorulainsagheer1234@gmail.com>
2026-02-10 14:29:23 +0500

    Create Q3-Formatted-Output.cpp

---
+++ Semester-1/Programming-Fundamentals/Lab-Tasks/Q3-Formatted-Output.cpp
@@ -0,0 +1,22 @@
+//Your need to print the formatted output like shown below: 
+ 
+//##########################  
+//$$$$*************************$$$$  
+//********\\\WELCOME to CUST******* 
+//$$$$*************************$$$$  
+//########################## 
+
+//Code:
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "##########################" << endl;
+    cout << "$$$$*************************$$$$" << endl;
+    cout << "********\\\\\\WELCOME to CUST*******" << endl;
+    cout << "$$$$*************************$$$$" << endl;
+    cout << "##########################" << endl;
+
+    return 0;
+}
